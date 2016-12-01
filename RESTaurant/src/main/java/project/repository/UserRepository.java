48663c903eb105e787f4d2db20e57f3a83609cfc
Bojan Batalo <bbatalo@hotmail.com>
2016-12-01 23:11:52 +0100

    Početna verzija projekta. Sadrži samo primere funkcionalnosti konekcije sa bazom, primer stek arhitekture i REST servera.

---
+++ RESTaurant/src/main/java/project/repository/UserRepository.java
@@ -0,0 +1,16 @@
+package project.repository;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.Repository;
+
+import project.domain.User;
+
+public interface UserRepository extends Repository<User, Long> {
+
+	public Page<User> findAll(Pageable pageable);
+	
+	@Query("select u from User u where u.email = ?1")
+	public User findUser(String email);
+}
---
+++ RESTaurant/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into User (name, surname, email, password) values ('Bobi', 'Bobinson','bobi@bobinjo.com', 'bobijevasifra')
+insert into User (name, surname, email, password) values ('Prix', 'Prixinson','prix@bobinjo.com', 'prixevasifra')
+insert into User (name, surname, email, password) values ('Gagi', 'Idiot','gagi@bobinjo.com', 'gagijevasifra')
